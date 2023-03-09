275e38128bcdc845f34b825c42d2a0284802054e
Ivan Lizzt <alejandrolistc@hotmail.com>
2023-03-09 16:12:22 -0500

    Database connection
    Post and Get methods for Student model

--- src/main/java/com/alexlizzt/school/service/StudentService.java
+++ src/main/java/com/alexlizzt/school/service/StudentService.java
@@ -1,4 +1,10 @@
 package com.alexlizzt.school.service;
 
+import com.alexlizzt.school.model.Student;
+
+import java.util.List;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
--- src/main/java/com/alexlizzt/school/service/StudentServiceImpl.java
+++ src/main/java/com/alexlizzt/school/service/StudentServiceImpl.java
@@ -1,4 +1,25 @@
 package com.alexlizzt.school.service;
 
-public class StudentServiceImpl {
+import com.alexlizzt.school.model.Student;
+import com.alexlizzt.school.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
